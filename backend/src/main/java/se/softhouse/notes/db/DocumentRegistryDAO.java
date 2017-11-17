fb9c9b77747a3308f95dd52bcc565a4b100a5a1e
RaviShankarKondoju <ravishankar.elite@gmail.com>
2017-11-17 09:04:04 +0100

    Changed database name

--- backend/src/main/java/se/softhouse/notes/db/DocumentRegistryDAO.java
+++ backend/src/main/java/se/softhouse/notes/db/DocumentRegistryDAO.java
@@ -14,25 +14,25 @@ import java.util.Optional;
 @RegisterMapperFactory(BeanMapperFactory.class)
 public interface DocumentRegistryDAO {
 
-    @SqlUpdate("CREATE TABLE IF NOT EXISTS `document_registry` (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +  //
+    @SqlUpdate("CREATE TABLE IF NOT EXISTS `doc_registry` (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +  //
             "title VARCHAR(12) NOT NULL, " + //
             "text VARCHAR(255) NOT NULL, " + //
             "header_name VARCHAR(255) NOT NULL)")
     void createTable();
 
-    @SqlQuery("select * FROM `document_registry`")
+    @SqlQuery("select * FROM `doc_registry`")
     List<DocumentRegistry> selectAll();
 
-    @SqlQuery("select * FROM `document_registry` WHERE id = :id")
+    @SqlQuery("select * FROM `doc_registry` WHERE id = :id")
     Optional<DocumentRegistry> get( @Bind("id") int id);
     
-    @SqlUpdate("INSERT INTO `document_registry` (title, text, header_name) VALUES(:title, :text, :title || ' ' ||  datetime('now', 'localtime') || ' ' || random() )")
+    @SqlUpdate("INSERT INTO `doc_registry` (title, text, header_name) VALUES(:title, :text, :title || ' ' ||  datetime('now', 'localtime') || ' ' || random() )")
     void insert(@BindBean DocumentRegistry documentRegistry);
 
-    @SqlUpdate("UPDATE `document_registry` set title = :title, text = :text, header_name = :title || ' ' || datetime('now', 'localtime') || ' ' || random() WHERE id = :id")
+    @SqlUpdate("UPDATE `doc_registry` set title = :title, text = :text, header_name = :title || ' ' || datetime('now', 'localtime') || ' ' || random() WHERE id = :id")
     void update(@BindBean DocumentRegistry documentRegistry);
 
-    @SqlUpdate("DELETE FROM `document_registry` WHERE id = :id")
+    @SqlUpdate("DELETE FROM `doc_registry` WHERE id = :id")
     void delete(@Bind("id") int id);
 
 
