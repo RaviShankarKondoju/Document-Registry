a899cd0a42c2b9970cedc52108f6a35f31a366c4
RaviShankarKondoju <33348004+RaviShankarKondoju@users.noreply.github.com>
2018-02-10 18:29:28 +0100

    Update NoteException.java

--- backend/src/main/java/se/softhouse/notes/exceptions/NoteException.java
+++ backend/src/main/java/se/softhouse/notes/exceptions/NoteException.java
@@ -2,9 +2,6 @@ package se.softhouse.notes.exceptions;
 
 import javax.ws.rs.WebApplicationException;
 
-/**
- * @todo: this class should be renamed to 'MyProjectException' for your projects
- */
 public class NoteException extends WebApplicationException {
     public NoteException(int status) {
         super(status);
